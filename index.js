a6f760882de792fd07abfc39e766b303faa6a131
박영민 <YoungMin@DOCUui-MacBook-Pro.local>
2022-02-08 10:23:27 +0900

    index.js 주석제거

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const cron = require('node-cron');
 const oidSearch = require ("./search.js");
 const fs = require("fs");
 
-/*
-oidSearch.oidSearch('ApeosPort-VI C3371', '','')
-.then(function(result){
-	var aaa = JSON.parse(result[0])
-})
-*/
-//const jsonFile = fs.readFileSync('./config/equipmentList2.json', 'utf8');
-//const jsonData = JSON.parse(jsonFile);
-//console.log(jsonData);
-
 webStart.webStart();
 agent.agentStart()
 
@@ -24,61 +14,3 @@ var task = cron.schedule('* * * * *', function () {
 });
 
 task.start();
-/*
-function valueSet(oidArr){
-	var oidValue = "";
-	var session = snmp.createSession ("192.168.0.199", "public");
-	session.get (oidArr, function (error, varbinds) {
-		if (error) {
-			oidValue = "N";
-		} else {
-			for (var i = 0; i < varbinds.length; i++) {
-				if (snmp.isVarbindError (varbinds[i])) {
-					console.error (snmp.varbindError (varbinds[i]));
-				} else {
-					console.log (varbinds[i].oid + " = " + varbinds[i].value);
-					oidValue =  varbinds[i].value;
-				}
-			}
-		}
-		return oidValue;
-		session.close ();
-	});
-}
-var eArr = new Array();
-var sJson = "";
-function oidSet(oids){
-	var session = snmp.createSession ("192.168.0.199", "public");
-	
-	session.get (oids, function (error, varbinds) {
-		if(error){
-			console.error (error);
-		}else{
-			for(var i = 0; i < varbinds.length; i++) {
-				if(snmp.isVarbindError (varbinds[i])){
-					console.error (snmp.varbindError (varbinds[i]));
-				}else{
-					if(varbinds[i].oid == "1.3.6.1.2.1.1.5.0"){
-						var model_name = varbinds[i].value;
-					}
-					if(varbinds[i].oid == "1.3.6.1.2.1.43.5.1.1.17.1"){
-						var serial_num = varbinds[i].value;
-					}
-				}
-			}
-			search.oidSearch(model_name.toString(), serial_num.toString(), "192.168.0.199")
-				.then(function(result){
-				console.log(result);
-			})
-		}
-		
-		session.close ();
-	});
-
-	session.trap (snmp.TrapType.LinkDown, function (error) {
-		if (error) {
-			console.error (error);
-		}
-	});
-}
-*/
